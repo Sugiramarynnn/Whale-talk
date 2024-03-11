885e14c06cb3aec47922283d8855b635bc999f3f
Sugiramarynnn <sugimarylene@gmail.com>
2024-03-11 05:44:55 +0200

    first commit

---
+++ whaletalk.js
@@ -0,0 +1,15 @@
+let input= "girl";
+const vowels=['a','e','i','o','u'];
+
+const resultArray=['g','i','r','r','l'];
+
+array.forEach(element => {
+    input.length;
+}); 
+
+for(input.length===22; ; ){
+    console.log('i is'+i);
+    for(vowels===5; ;){
+        console.log('j is'+j);
+    }
+}
